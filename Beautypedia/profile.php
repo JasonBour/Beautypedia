fbbc3a7b0ef56ca83d82f0972767aa761c15bc02
JasonBour <675942652@qq.com>
2015-09-19 22:14:56 +0800

    some changes
    the main content and head image

--- Beautypedia/profile.php
+++ Beautypedia/profile.php
@@ -11,16 +11,17 @@
 <title></title>
 <meta charset="utf-8">
 <link rel="stylesheet" type="text/css" href="./css/profile.css">
+ <link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
 </head>
 
 <body >
-<div class="head" style="background: url('images/background.jpg');">
+<div class="head" style="background: url('images/image_head.png');">
 <section style="display:inline;float:left;">
   <header>
-   <ul>
-    <li style="list-style-type: none;display:inline;">Agents</li>
-    <li style="list-style-type: none;display:inline;">Login</li>
-    <li  style="list-style-type: none;display:inline;">Signup</li>
+   <ul style="color: white">
+    <li  class="header" style="list-style-type: none;display:inline;">Agents</li>
+    <li class="header" style="list-style-type: none;display:inline;">Login</li>
+    <li class="header" style="list-style-type: none;display:inline;">Signup</li>
    </ul>
   </header>
 
@@ -29,7 +30,7 @@
 
 
 <form action="" style="display:inline;float:left;"><input type="text" placeholder="Search" class="search">
-<input type="button" class="go" value="Go!">
+<input type="button" class="go" value="Go!" style="color: white">
 </form>
 
 </div>
@@ -37,20 +38,74 @@
 
 <div class="left-profile">
 
-<img alt="" src="./images/#"><!-- headimage circle -->
+<img alt="" src="./images/head.jpg" class="img-circle head" style="width: 50%;height: auto"><!-- headimage circle -->
 
 
 <!-- text here -->
-<h3>Relax,find something fun here </h3>
-<h4>I am just kidding ,ha</h4>
+<h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum facere, vero nisi ipsam dolores, architecto ad libero. Maxime, natus voluptatum saepe quam mollitia rem asperiores nostrum. Culpa ipsam voluptas nesciunt! </h3>
+<h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius alias sequi dignissimos, sed aspernatur quas? Est temporibus culpa ipsa, quisquam doloremque incidunt expedita quas, suscipit officiis reiciendis ipsam mollitia quos.</h4>
 
 
 </div>
 
 
 <div class="center-news">
-<div class="head-info"></div> <!-- this is the lastest news number for users  -->
-<div class="real-content"></div>
+
+<div class="head-info">
+<section class="number">
+	<ul>
+		<li class="item">
+			<span class="glyphicon glyphicon-thumbs-up" aria-hidden="true" >
+             <font style="font-family:'Open Sans', Helvetica, Arial, sans-serif;">点赞次数</font>
+             </span>
+             <p>
+             	200
+             </p>
+
+		</li>
+
+
+		 <li class="item">
+		 	<span class="glyphicon glyphicon-heart" aria-hidden="true" >
+             <font style="font-family:'Open Sans', Helvetica, Arial, sans-serif;">喜欢次数</font>
+             </span>
+             <p>
+             	200
+             </p>
+		 </li >
+
+        <li class="item">
+		 	<span class="glyphicon glyphicon-facetime-video" aria-hidden="true" >
+             <font style="font-family:'Open Sans', Helvetica, Arial, sans-serif;">视频数量</font>
+             </span>
+             <p>
+             	130
+             </p>
+		 </li >
+
+		  <li class="item">
+		 	<span class="glyphicon glyphicon-picture" aria-hidden="true" >
+             <font style="font-family:'Open Sans', Helvetica, Arial, sans-serif;">照片数量</font>
+             </span>
+             <p>
+             	100
+             </p>
+		 </li >
+ 
+
+ 
+
+	</ul>
+</section>
+
+
+</div> <!-- this is the lastest news number for users  -->
+<div class="divide">
+
+</div>
+<div class="real-content">
+
+</div>
 </div>
 
 
